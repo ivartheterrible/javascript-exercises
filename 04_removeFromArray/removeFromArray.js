eb3cad4a1f32443506209dab1195452aecd5103c
ddr <mrdrifter123@outlook.com>
2024-07-06 18:43:40 +1000

    finished 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
+
+    array.forEach((item) => {
+        // push every element into the new array
+        // UNLESS it is included in the function arguments
+        // so we create a new array with every item, except those that should be removed
+        if (!args.includes(item)) {
+          newArray.push(item);
+        }
+      });
+
+    return newArray;
 };
 
 // Do not edit below this line
