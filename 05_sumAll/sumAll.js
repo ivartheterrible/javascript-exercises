b46839fb1985beeaf696ad7eccf47eaf41210930
ddr <mrdrifter123@outlook.com>
2024-07-07 13:11:17 +1000

    finished 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,34 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (typeof(num1) != "number" || typeof(num2) != "number"){
+        return "ERROR";
+    } else if (num1 < 0 || num2 < 0){
+        return "ERROR";
+    }
+    
+    let total = 0;
+    let numArray = [];
+    let bigNum = 0;
+    let smallNum = 0;
 
+    if(num1 < num2){
+        smallNum = num1;
+        bigNum = num2;
+    } else if(num1 > num2){
+        smallNum = num2;
+        bigNum = num1;
+    }
+
+    for(i=smallNum; i<= bigNum; i++){
+        numArray.push(i);
+    }
+
+    function forEachNum(item){  
+        total += item;
+    }
+
+    numArray.forEach(forEachNum)
+
+    return total;
 };
 
 // Do not edit below this line
