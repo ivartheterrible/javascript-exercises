e5d5e55fcca5044285effeeeb5c4d8acaf0bb029
ddr <mrdrifter123@outlook.com>
2024-07-18 18:25:02 +1000

    asdf

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let arrayValues = string.split('');
+    let reverseArrayValues = arrayValues.reverse();
+    let reverseString = reverseArrayValues.join('');
 
+    if (string === reverseString){
+        return true;
+    } else{
+        return false;
+    }
 };
 
 // Do not edit below this line
